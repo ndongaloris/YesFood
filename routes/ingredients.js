e91c6b27a69643bf1d292908d0e04b3a3c6f8a89
ndongaloris <ndongajared@gmail.com>
2024-06-17 16:11:36 +0200

    rest link and route adjusted

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -5,6 +5,6 @@ router.get("/", ingredientController.getAll);
 router.get("/:id", ingredientController.getSingle);
 router.post("/create", ingredientController.createIngredient);
 router.put("/update/:id", ingredientController.updateIngredient);
-router.delete("/:id", ingredientController.deleteIngredient);
+router.delete("/delete/:id", ingredientController.deleteIngredient);
 
 module.exports = router;
--- routes/recipes.js
+++ routes/recipes.js
@@ -4,7 +4,7 @@ const recipeController = require("../controllers/recipes");
 router.get("/", recipeController.getAll);
 router.get("/:id", recipeController.getSingle);
 router.post("/create", recipeController.createRecipe);
-router.put("/:id", recipeController.updateRecipe);
-router.delete("/:id", recipeController.deleteRecipe);
+router.put("/update/:id", recipeController.updateRecipe);
+router.delete("/delete/:id", recipeController.deleteRecipe);
 
 module.exports = router;
